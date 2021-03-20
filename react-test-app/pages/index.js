645e1a94d3284f4f5d903d69521862c5a9e8492a
max <petrow-wodcin@yandex.ru>
2021-03-21 00:17:49 +0300

    Add table

--- react-test-app/pages/index.js
+++ react-test-app/pages/index.js
@@ -1,13 +1,55 @@
+import {useState, useEffect} from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import TableStyles from '../components/table.module.css'
+import Date from '../components/date'
 
 const siteTitle = 'Table with documents'
 
-export default function Home() {
+export default function Docs({ docs }){
+  const [docs, setDocs] = useState(initialState: [])
+  
+  useEffect( effect: () => {
+    async function load() {
+      const response = await fetch (input: 'http://localhost:4200/document') 
+      const json = await response.json()
+      setDocs(json)
+    }
+    
+    load()
+    
+  }, inputs: [])
+  
   return (
     <Head>
-      <title> {siteTable} </title>
+      <title>{siteTitle}</title> 
     </Head>
-    
+    <table className=TableStyles.table>
+      <thead>
+        <tr>
+          <th className=TableStyles.th scope="col">#</th>
+          <th className=TableStyles.th scope="col">Date</th>
+          <th className=TableStyles.th scope="col">Name</th>
+        <tr>
+      </thead>
+      <tbody>
+        {docs.map( doc => (
+          <tr>
+            <th className=TableStyles.th scope="row">{doc.id}</th>
+            <th className=TableStyles.th scope="col"><Date dateString={doc.date} /></th>
+            <th className=TableStyles.th scope="col">{doc.displayName}</th>
+          <tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
+
+Docs.getInitialProps = async() => {
+  const response = await fetch (input: 'http://localhost:4200/document') 
+  const docs = await response.json()
+  
+  return {
+    docs
+  }
+}
+
