7086b6c7c45babc61da57c31db30cd89cdfc5c4e
max <petrow-wodcin@yandex.ru>
2021-03-21 23:31:35 +0300

    Add document info

---
+++ react-test-app/pages/doc/[id].js
@@ -0,0 +1,22 @@
+import {Layout} from '../../components/layout'
+import Date from '../components/date'
+
+export default function Doc( {doc} ) {
+  return( 
+    <Layout>
+      <h1><Date dateString={doc.docDate} /></h1>
+      <h1>{doc.displayName}</h1>
+      <h1>{doc.description}</h1>
+    </Layout>
+  )
+}
+
+Doc.getInitialProps = async (ctx) => {
+  
+  const response = await fetch (`http://localhost:4200/document/${ctx.query.id}` ) 
+  const doc = await response.json()
+  
+  return {
+    doc
+  }
+}
