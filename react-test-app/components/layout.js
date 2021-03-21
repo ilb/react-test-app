dbcf8b1dff7f015e7a8229f20d4a4494e28e30af
max <petrow-wodcin@yandex.ru>
2021-03-22 00:04:53 +0300

    Add home link

--- react-test-app/components/layout.js
+++ react-test-app/components/layout.js
@@ -1,12 +1,22 @@
-export function Layout({ children }) {
+import styles from './layout.module.css'
+import Link from 'next/link'
+
+export function Layout({ children, home }) {
   return (
-    <>
+    <div className={styles.container}>
       <nav>
-        <h1>Documents</h1>
+        <h1 className={styles.header}>Documents</h1>
       </nav>
       <main>
         {children}
       </main>
-    </>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
   )
 }
