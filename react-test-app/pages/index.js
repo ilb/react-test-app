684cb090ddf955e7c3006353214800e61eebf6d4
max <petrow-wodcin@yandex.ru>
2021-03-21 18:46:32 +0300

    Change <div> to <Layout>

--- react-test-app/pages/index.js
+++ react-test-app/pages/index.js
@@ -2,54 +2,45 @@ import {useState, useEffect} from 'react'
 import Head from 'next/head'
 import TableStyles from '../components/table.module.css'
 import Date from '../components/date'
+import {Layout} from '../components/layout'
 
 const siteTitle = 'Table with documents'
 
-export default function Docs({ docs }){
-  const [docs, setDocs] = useState(initialState: [])
-  
-  useEffect( effect: () => {
-    async function load() {
-      const response = await fetch (input: 'http://localhost:4200/document') 
-      const json = await response.json()
-      setDocs(json)
-    }
-    
-    load()
-    
-  }, inputs: [])
-  
+export default function Docs({ docs }){  
   return (
-    <Head>
-      <title>{siteTitle}</title> 
-    </Head>
-    <table className=TableStyles.table>
-      <thead>
-        <tr>
-          <th className=TableStyles.th scope="col">#</th>
-          <th className=TableStyles.th scope="col">Date</th>
-          <th className=TableStyles.th scope="col">Name</th>
-        <tr>
-      </thead>
-      <tbody>
-        {docs.map( doc => (
-          <tr>
-            <th className=TableStyles.th scope="row">{doc.id}</th>
-            <th className=TableStyles.th scope="col"><Date dateString={doc.date} /></th>
-            <th className=TableStyles.th scope="col">{doc.displayName}</th>
-          <tr>
-        ))}
-      </tbody>
-    </table>
+    <Layout>
+      <Head>
+        <title>{siteTitle}</title> 
+      </Head>
+      <section>
+        <table className={TableStyles.table}>
+          <thead>
+            <tr>
+              <th className={TableStyles.th} scope="col">#</th>
+              <th className={TableStyles.th} scope="col">Date</th>
+              <th className={TableStyles.th} scope="col">Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {docs.map( doc => (
+              <tr>
+                <th className={TableStyles.th} scope="row">{doc.id}</th>
+                <th className={TableStyles.th} scope="col"><Date dateString={doc.docDate} /></th>
+                <th className={TableStyles.th} scope="col">{doc.displayName}</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </Layout>
   )
 }
 
-Docs.getInitialProps = async() => {
-  const response = await fetch (input: 'http://localhost:4200/document') 
+Docs.getInitialProps = async () => {
+  const response = await fetch ('http://localhost:4200/document' ) 
   const docs = await response.json()
   
   return {
     docs
   }
 }
-
